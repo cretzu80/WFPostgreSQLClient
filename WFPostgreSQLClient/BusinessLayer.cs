b98794cf20fc416dc1ed9527b715b9ef828950ce
cretzu80 <stuparudaniel@gmail.com>
2015-10-05 22:24:24 +0300

    all 1

---
+++ WFPostgreSQLClient/BusinessLayer.cs
@@ -0,0 +1,43 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFPostgreSQLClient
+{
+    class BusinessLayer
+    {
+        private string m_connectionString = string.Empty;
+        internal BusinessLayer(string connectionString)
+        {
+            m_connectionString = connectionString;
+        }
+
+        internal DataTable GetDatabases()
+        {
+            DataTable dataTable = new DataTable();
+            using (NpgsqlConnection con = new NpgsqlConnection(m_connectionString))
+            {
+                NpgsqlDataAdapter adapter = new NpgsqlDataAdapter("SELECT * FROM pg_database WHERE datistemplate = false", con);
+                adapter.Fill(dataTable);
+                con.Close();
+            }
+            return dataTable;
+        }
+
+        internal object ExecuteBatch(string sqlCommand)
+        {
+            DataTable dataTable = new DataTable();
+            using (NpgsqlConnection con = new NpgsqlConnection(m_connectionString))
+            {
+                NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(sqlCommand, con);
+                adapter.Fill(dataTable);
+                con.Close();
+            }
+            return dataTable;
+        }
+    }
+}
